46b6f325838a53552e3c3b9f8d644f2acc056aed
Ntk-Fuse <ntk.fuse07@gmail.com>
2025-10-10 12:43:58 +0700

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -36,7 +36,7 @@ GO
 IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
-	ss_ord_num NVARCHAR(50),
+	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
 	sls_order_dt INT,
