1bfcdbac09ff94f6ea5d2c1b2f7ccd129b8f3717
Ntk-Fuse <ntk.fuse07@gmail.com>
2025-10-12 11:11:29 +0700

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,11 +1,13 @@
 /*
-===========================================
-CREATE ALL NECCESSARY TABLE IN BRONZE LAYER
-===========================================
-this script will **DROP TABLE** if it already exist and create a new one so make sure you really intent to run this script or had datas backed up already
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
 */
-
-
 
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
